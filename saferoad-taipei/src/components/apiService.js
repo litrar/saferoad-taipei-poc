e5e26e4b3f9fcf43349cd6c0874cf44406b11f1f
litrar <ninghsuan1818@gmail.com>
2025-01-08 17:07:02 +0800

    redirect

--- saferoad-taipei/src/components/apiService.js
+++ saferoad-taipei/src/components/apiService.js
@@ -1,6 +1,6 @@
 // apiService.js
 export async function fetchRoute(sourceLat, sourceLng, destLat, destLng) {
-    const apiUrl = "/api/route";
+    const apiUrl = "api/route";
     const params = {
       source_lat: sourceLat,
       source_long: sourceLng,
